c385f9afb06e7842d91765fae1a98ba1ff6bd4f3
Reyner Veliz <reynerv@yahoo.com>
2022-09-08 22:04:37 -0600

    Create App Component as parent for all

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({id, name, email}) => {
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow">
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow">
             <img src={`https://robohash.org/${id}?set=set2&size=200x200`} alt="robot card" />
             <div>
                 <h2>{name}</h2>
