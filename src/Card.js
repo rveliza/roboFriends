73e04a4fbdcd525bc485aeef769a0e3ac25caa36
Reyner Veliz <reynerv@yahoo.com>
2022-09-08 19:46:46 -0600

    Add props to Cards

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (prop) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow">
-            <img src="https://robohash.org/katia?set=set4&size=200x200" alt="robot card" />
+            <img src={`https://robohash.org/${prop.id}?set=set4&size=200x200`} alt="robot card" />
             <div>
-                <h2>Katia Veliz</h2>
-                <p>katia.veliz@gmail.com</p>
+                <h2>{prop.name}</h2>
+                <p>{prop.email}</p>
             </div>
         </div>
     );
