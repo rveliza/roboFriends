2a75186a8211e4e56d6c080c5bec9222a2a381db
Reyner Veliz <reynerv@yahoo.com>
2022-09-08 19:23:58 -0600

    Create first Card

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/katia?set=set4&size=200x200" alt="robot card" />
+            <div>
+                <h2>Katia Veliz</h2>
+                <p>katia.veliz@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
