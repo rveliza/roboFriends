ca32eca6fbed43ea2a6e05a2f9f88eab9f9288dc
Reyner Veliz <reynerv@yahoo.com>
2022-09-08 22:43:19 -0600

    Create App class that extends from Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import Searchbox from './SearchBox.js';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <Searchbox />
-            <CardList robots={ robots }/>
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    render() {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <Searchbox />
+                <CardList robots={ this.state.robots }/>
+            </div>
+        )
+    }
 }
 
 export default App;
